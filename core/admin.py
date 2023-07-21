71d7362453b05f2688d2c0b91a0746598a651de4
gabrielnm123 <gabrielnmatos96@gmail.com>
2023-07-20 21:54:24 -0300

    criado classes Acomplanhante e Paciente

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from core.models import Paciente, Acompanhante
 # Register your models here.
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('paciente', 'data_registro_paciente', 'usuario') # pra aparecer no resgistro do paciente logo de cara
+    list_filter = ('paciente', 'data_registro_paciente', 'usuario') # pra filtrar
+
+class AcompanhanteAdmin(admin.ModelAdmin):
+    list_display = ('paciente', 'acompanhante', 'data_registro_acompanhante', 'usuario')
+    list_filter = ('paciente', 'acompanhante', 'data_registro_acompanhante', 'usuario')
+
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Acompanhante, AcompanhanteAdmin)
