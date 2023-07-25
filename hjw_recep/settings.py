3ba77b742a354eda02fd16759f784b32afa30b6b
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2023-07-25 23:49:30 +0000

    testando django-autocomplete-light==3.9.7 e django-smart-selects==1.6.0

--- hjw_recep/settings.py
+++ hjw_recep/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "smart_selects" # adicionar app da biblioteca smart_selects
+    "smart_selects", # adicionar app da biblioteca smart_selects
+    'dal',
+    'dal_select2'
 ]
 
 MIDDLEWARE = [
