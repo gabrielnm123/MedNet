5bec1dac783e7a50d662145f1d6079094189bef5
Gabriel <gabrielnmatos@gmail.com>
2023-08-17 16:40:40 -0300

    consertando

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from dal import autocomplete
 from core.models import *
-from django.http.response import Http404, JsonResponse # http404 pra implicar esse erro, jsanresponse pra trabalhar com javascript
+from django.http.response import Http404
 import json
 
 # Create your views here.
