18a287d722429e1a967c6df8a28f77e916841edd
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2023-08-18 15:22:32 +0000

    mudando o debug pra false

--- hjw_recep/settings.py
+++ hjw_recep/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s3@^1rn&cwt+%(%r!)a*6*28v9x3jk@ieyc7f*f$abz=300s*e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
