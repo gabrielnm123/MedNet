2d66e9f12ae0fc529afe5176df16f1ac47c53084
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2023-09-01 12:43:56 +0000

    generalizando STATICFILES_DIRS

--- hjw_recep/settings.py
+++ hjw_recep/settings.py
@@ -144,7 +144,6 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/home/gabriel/workspace/hjw_recep/static'
 ]
 
 # Configurar o local onde os arquivos estáticos serão coletados para produção
