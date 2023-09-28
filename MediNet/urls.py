9f19eff5aa0a6d86b569a4afd487e9beebbf1a0f
gabrielnm123 <gabrielnmatos96@gmail.com>
2023-09-28 13:07:32 -0300

    projeto vai ter nome proprio, sem vinculo com instituição alguma

--- MediNet/urls.py
+++ MediNet/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hdgmjw project.
+URL configuration for MediNet project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
