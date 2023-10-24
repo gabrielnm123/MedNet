bc01782934d26bf2afff08dd7d549458e092f240
gabrielnm123 <gabrielnmatos96@gmail.com>
2023-10-24 09:31:48 -0300

    teste docker funcionou

--- MedNet/settings.py
+++ MedNet/settings.py
@@ -91,7 +91,7 @@ if is_server:
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE'),
-        'HOST': '0.0.0.0',
+        'HOST': '172.17.0.5',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD'),
