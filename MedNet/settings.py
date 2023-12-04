9b2f891e6c63f86c567d07832ba98ce5eae60c16
Gabriel <gabrielnmatos@gmail.com>
2023-12-04 06:06:17 -0300

    novo banco online

--- MedNet/settings.py
+++ MedNet/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Define qual banco de dados vai usar, container docker ou da plataforma planetscale, se True é do container
-DATABASES_IS_DOCKER = True
+DATABASES_IS_DOCKER = False
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
